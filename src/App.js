2eed2538431c7f1e7fee694adec07c0aa1f2f06c
Amogh Warkhandkar <amoghw2025@gmail.com>
2021-10-25 15:11:36 +0530

    Init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="w-screen h-screen flex items-center justify-center">App</p>
+    </div>
+  );
+};
+
+export default App;
